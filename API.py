db928da3b85c37e1c2be56ef2c117e0390840dcc
lanserman <89411025+lane-jr@users.noreply.github.com>
2026-05-24 18:27:41 -0500

    api logic first setup

--- API.py
+++ API.py
@@ -1,8 +1,40 @@
 import fastapi
 import requests
-import uvicorn
+import os
+from datetime import datetime
+
 from dotenv import load_dotenv
 
-print("dotenv works")
+load_dotenv()
 
-#API_KEY
+#jarvis station id
+#Jarvis = "41190"
+
+api_key = os.getenv("API_KEY")
+
+url = (
+    "https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx"
+    f"?key={api_key}"
+    "&mapid=41190"
+    "&outputType=JSON"
+)
+
+response = requests.get(url)
+print(response.status_code)
+
+train_data = response.json()
+
+arrival_times = train_data["ctatt"]["eta"][0]
+
+if arrival_times["isApp"] == "1":
+    print("Due")
+
+else:
+    arr_time = datetime.fromisoformat(arrival_times["arrT"])
+    current_time = datetime.now(arr_time.tzinfo)
+
+    min = round(
+        (arr_time - current_time).total_seconds() / 60
+    )
+    
+    print(f"{min} min")
